ed13b0e68573dfc5d6b623f6bee554d56a8692c5
Josh Wilson <joshua.wilson@vanderbilt.edu>
2018-07-23 11:07:02 -0500

    Added placeholder values for mac addresses to Model_* classes.

--- starLib/src/main/java/edu/illinois/mitra/starl/models/Model_Minidrone.java
+++ starLib/src/main/java/edu/illinois/mitra/starl/models/Model_Minidrone.java
@@ -60,8 +60,8 @@ public class Model_Minidrone extends Model_Drone {
 	public double max_yaw_speed() { return 200; }
 
 	@Override
-	public DroneInterface createModelInterface(GlobalVarHolder gvh, Context context, String mac) {
-		return new MiniDroneInterface(gvh, context, mac);
+	public DroneInterface createModelInterface(GlobalVarHolder gvh, Context context) {
+		return new MiniDroneInterface(gvh, context, "TODO PLACEHOLDER");
 	}
 
 	@Override
